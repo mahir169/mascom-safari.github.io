f1529af790fc4b05ea0aba617cd7de97536baf09
mahir169 <mahirabrahman18@gmail.com>
2024-12-10 16:25:30 +0300

    Add files via upload

---
+++ assets/js/mc-script.js
@@ -0,0 +1,8 @@
+/** sidebar */
+function openNav() {
+    document.getElementById("myNav").style.height = "40%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+}
